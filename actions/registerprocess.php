6015c0460d3920fba9bd1f386a535f4521a59b06
ADjan2023 <banyin.djan@ashesi.edu.gh>
2022-12-05 22:50:04 +0000

    Update registerprocess.php

--- actions/registerprocess.php
+++ actions/registerprocess.php
@@ -15,13 +15,18 @@ $city=$_POST['city'];
 $contact=$_POST['contact'];
 $role=2;
 
-if(add_customer_ctr($name,$email,$password,$country,$city,$contact,$role)){
-	echo "hi";
-} else if(add_customer_ctr($name,$email,$password,$country,$city,$contact,$role)==false){
-	echo "bye";
-}
-}
-	/*if ( $unencryptpass==$confirmpass) {
+
+	if ( $unencryptpass===$confirmpass) {
+		$name=$_POST['fullname'];
+$email=$_POST['email'];
+$unencryptpass=$_POST['password'];
+$confirmpass=$_POST['password2'];
+$password=password_hash($unencryptpass, PASSWORD_DEFAULT); 
+$country=$_POST['country'];
+$city=$_POST['city'];
+$contact=$_POST['contact'];
+$role=2;
+
 
 		if(add_customer_ctr($name,$email,$password,$country,$city,$contact,$role)==TRUE){
 			header('Location:../Login/login.php');
@@ -37,7 +42,7 @@ if(add_customer_ctr($name,$email,$password,$country,$city,$contact,$role)){
 		$_SESSION['error'] = 'Unable to register user!';		
 		header('Location:../Login/register.php');
 	}
-}*/
+}
 else {
 	
 	header('Location:../Login/register.php');
