4168827883fe276dc60d1e7973ffc9d5a1feb3d4
ADjan2023 <banyin.djan@ashesi.edu.gh>
2022-12-06 03:25:29 +0000

    done

--- Adverts/index.php
+++ Adverts/index.php
@@ -82,7 +82,7 @@
     currency:'GHS',
      ref: ''+Math.floor((Math.random() * 1000000000) + 1),
     onClose: function(){
-    alert('Window closed.');
+    
     },
     callback: function(response){
           
